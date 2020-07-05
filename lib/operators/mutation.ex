a87c0f057b709943fe3cbe73914bf9b4bdbe6c5a
Marius Kalvø <mariuskalvo30@gmail.com>
2020-07-05 14:27:08 +0200

    Refactored mutation operator

--- lib/operators/mutation.ex
+++ lib/operators/mutation.ex
@@ -1,25 +1,27 @@
 defmodule Mutation do
 
+  defp apply_mutation(individual) do
+    individual_length = length(individual)
+    index1 = trunc(Adapters.Random.uniform_max(individual_length) - 1)
+    index2 = trunc(Adapters.Random.uniform_max(individual_length) - 1)
+
+    value1 = Enum.at(individual, index1)
+    value2 = Enum.at(individual, index2)
+
+    individual
+    |> Enum.to_list
+    |> List.replace_at(index1, value2)
+    |> List.replace_at(index2, value1)
+  end
+
   @spec mutate([integer], number) :: [integer]
   def mutate(individual, mutation_probability) do
     should_mutate = mutation_probability > Adapters.Random.uniform()
 
-    if (!should_mutate) do
-      individual
+    if should_mutate do
+      apply_mutation(individual)
     else
-      individual_length = length(individual)
-
-      index1 = trunc(Adapters.Random.uniform_max(individual_length) - 1)
-      index2 = trunc(Adapters.Random.uniform_max(individual_length) - 1)
-
-      value1 = Enum.at(individual, index1)
-      value2 = Enum.at(individual, index2)
-
       individual
-      |> Enum.to_list
-      |> List.replace_at(index1, value2)
-      |> List.replace_at(index2, value1)
     end
   end
-
 end
